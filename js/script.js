62c4f2b203a22ea088d83f1bc75893b5f5e3333f
Adrien <helary.adrien@gmail.com>
2022-03-01 18:09:29 +0100

    Feature custom JS Add a toggle between light and dark theme Add a toggle for the hyperlegible  font

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const theme = document.getElementById('theme')
+const toggleTheme = document.getElementById('toggleTheme');
+const body = document.getElementById('body');
+const toggleFont = document.getElementById('toggleFont');
+
+toggleTheme.addEventListener('click', (e)=> {
+    if (e.target.classList.contains('bi-moon')){
+        e.target.classList.replace('bi-moon', 'bi-sun');
+        switchMode('dark');
+        return;
+    }
+    if (e.target.classList.contains('bi-sun')){
+        e.target.classList.replace('bi-sun', 'bi-moon');
+        switchMode('light');
+        return;
+    }
+    
+})
+
+toggleFont.addEventListener('click', (e)=> {
+    switchPolice();
+})
+
+
+function switchMode(mode){
+    const colors = {
+        light: './css/bootstrap.css',
+        dark: './css/bootstrap-dark.css'
+    }
+    theme.setAttribute('href', colors[mode])
+}
+
+function switchPolice(){
+    body.classList.toggle('dys');
+}
