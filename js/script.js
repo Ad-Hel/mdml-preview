ddd225beb36417cb4b30374dc4e224d1a33e3037
Adrien <helary.adrien@gmail.com>
2022-03-03 12:02:09 +0100

    Add a test to change icon if dark mode detected

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@ const toggleTheme = document.getElementById('toggleTheme');
 const body = document.getElementById('body');
 const toggleFont = document.getElementById('toggleFont');
 
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches){
+    toggleTheme.firstChild.classList.replace('bi-moon', 'bi-sun');
+}
+
 toggleTheme.addEventListener('click', (e)=> {
     if (e.target.classList.contains('bi-moon')){
         e.target.classList.replace('bi-moon', 'bi-sun');
